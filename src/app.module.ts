0d06ce0e54b9db62ebf51ff72ac4fecc69538a02
Leah Deshar <leah.desar@gmail.com>
2024-04-05 14:20:04 +0545

    nest js user and comment api

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,25 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { CommentsModule } from './comments/comments.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import config from './config/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://leahdesar:YIQsgaVrZBJhGUIL@cluster0.cnco2vy.mongodb.net/thread-nest',
-    ),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('database.conn'),
+      }),
+      inject: [ConfigService],
+    }),
     UsersModule,
     CommentsModule,
+    ConfigModule.forRoot({
+      cache: true,
+      isGlobal: true,
+      load: [config],
+    }),
     MongooseModule.forFeature([]),
   ],
   controllers: [AppController],
---
+++ src/config/config.ts
@@ -0,0 +1,5 @@
+export default () => ({
+  database: {
+    conn: process.env.CONNECTION,
+  },
+});
